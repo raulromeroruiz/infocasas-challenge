e6d99411dc8279ef0c4da7d823686f42df051a72
Raúl Romero <raul.romeroruiz@gmail.com>
2023-02-03 23:14:45 -0500

    feat: Enable cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -73,7 +73,8 @@ $app->configure('app');
 */
 
 $app->middleware([
-     App\Http\Middleware\ValidateMiddleware::class
+     App\Http\Middleware\CorsMiddleware::class,
+     App\Http\Middleware\ValidateMiddleware::class,
 ]);
 
 // $app->routeMiddleware([
